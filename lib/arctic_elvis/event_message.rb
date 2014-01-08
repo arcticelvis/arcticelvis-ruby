7c2ec99361715b5e34ee94a80f21a22470e92fd8
Christian Hager <persson.hager@gmail.com>
2014-01-08 22:44:07 +0100

    better specs for preview

--- lib/arctic_elvis/event_message.rb
+++ lib/arctic_elvis/event_message.rb
@@ -3,7 +3,8 @@ module ArcticElvis
     attr_accessor :id, :event_id, :subject, :preview, :state
 
     def preview(payload={})
-      response = ArcticElvis.request(:post, "events/#{event_id}/event_messages/#{id}/preview", payload)
+      response, status = ArcticElvis.request(:post, "events/#{event_id}/event_messages/#{id}/preview", payload)
+      raise ArcticElvis::RecordNotFoundError if status == 404
       response["event_message"]["preview"]
     end
   end
--- spec/arctic_elvis/event_message_spec.rb
+++ spec/arctic_elvis/event_message_spec.rb
@@ -3,15 +3,27 @@ require 'spec_helper'
 describe ArcticElvis::EventMessage do
   before do
     ArcticElvis.configure do |config|
-      config.api_key = '123'
+      config.api_key = 'cd242fb288ddf6e501decb92b84ac0a4'
     end
   end
 
   describe "preview" do
-    it "should fetch the preivew" do
-      ArcticElvis.should_receive(:request).with(:post, "events/1/event_messages/2/preview", {}).and_return("event_message" => {"preview" => "<b>Arctic</b>"})
-      event_message = ArcticElvis::EventMessage.new({id: "2", event_id: "1"})
-      event_message.preview.should eq "<b>Arctic</b>"
+    describe "failure" do
+      it "raises record not found exception if the event message is not found" do
+        VCR.use_cassette('event_message_preview_404') do
+          event_message = ArcticElvis::EventMessage.new({id: "2", event_id: "1"})
+          expect { event_message.preview }.to raise_exception ArcticElvis::RecordNotFoundError
+        end
+      end
+    end
+
+    describe "success" do
+      it "should render the payload in the event message and return a preview" do
+        VCR.use_cassette('event_message_preview_200') do
+          event_message = ArcticElvis::EventMessage.new({id: "235", event_id: "177"})
+          event_message.preview({payload: {name: 'Surly you have a name mister!'}}).should include "Surly you have a name mister!"
+        end
+      end
     end
   end
 end
