889255bef04be63711ffc127de72405b1aed81e7
Christian Hager <persson.hager@gmail.com>
2014-01-08 07:01:36 +0100

    remove client, dont need

--- spec/arctic_elvis/client_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-
-describe ArcticElvis::Client do
-  it "should inherit configuration" do
-    client = ArcticElvis::Client.new(api_key: '123')
-    client.api_key.should eq '123'
-  end
-end
